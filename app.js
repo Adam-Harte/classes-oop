30ff7d1a91092cdd2e8b6b46224b80c4affef447
Adam-Harte <adamharte11@hotmail.co.uk>
2020-03-27 13:01:16 +0000

    constructor method added

--- app.js
+++ app.js
@@ -6,3 +6,20 @@ class Product {
 
 const newProduct = new Product();
 console.log(newProduct);
+
+class Book {
+  title;
+  synopsis;
+  author;
+  releaseDate;
+
+  constructor(title, synopsis, author, releaseDate) {
+    this.title = title;
+    this.synopsis = synopsis;
+    this.author = author;
+    this.releaseDate = releaseDate;
+  }
+}
+
+const newBook = new Book('Mistborn', 'Allomancy', 'Brandon Sanderson', 2011);
+console.log(newBook);
